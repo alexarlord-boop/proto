d76296770e912c5cd392d4a0c976e67b5144b59d
aleksandr.petrunin <aleksandr.petrunin@jetbrains.com>
2025-10-11 19:06:27 +0300

    Feature: Add drag-and-drop canvas with draggable items using `@dnd-kit/core`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import { Button } from "@/components/ui/button"
+import { DnDCanvas } from "@/components/DnDCanvas"
 
 function App() {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button onClick={
-                async () => {
-                    try {
-                        const response = await fetch("http://127.0.0.1:8000/");
-                        const data = await response.json();
-                        console.log(data);
-                    } catch (error) {
-                        console.error(error);
-                    }
-                }
-            }>Click me</Button>
-        </div>
+        <DnDCanvas />
     )
 }
 
