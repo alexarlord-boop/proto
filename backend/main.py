7924b430464645a41ee9ed1814a659385e73671f
aleksandr.petrunin <aleksandr.petrunin@jetbrains.com>
2025-10-11 19:00:54 +0300

    Setup: Add FastAPI backend with CORS and integrate basic API call in frontend

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+async def test():
+    return {"message": "This is a test endpoint"}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,17 @@ import { Button } from "@/components/ui/button"
 function App() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
+            <Button onClick={
+                async () => {
+                    try {
+                        const response = await fetch("http://127.0.0.1:8000/");
+                        const data = await response.json();
+                        console.log(data);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+            }>Click me</Button>
         </div>
     )
 }
